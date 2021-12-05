aa19bc915b116d8baa0a7d4200c66169bc9db499
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2021-12-05 20:04:39 -0300

    colocando menu mobile para funcionar

--- index.js
+++ index.js
@@ -3,5 +3,12 @@ let e1 = document.querySelector('.menu-mobile-icon')
 e1.addEventListener('click', () => {
   
   let menuItens = document.querySelector('.menu-itens');
-  menuItens.classList.add('show');
+  if(menuItens.classList.contains('show')){
+    menuItens.classList.add('hidde');
+    menuItens.classList.remove('show');
+  }else{
+    menuItens.classList.add('show');
+    menuItens.classList.remove('hidde');
+  }
+  
 });

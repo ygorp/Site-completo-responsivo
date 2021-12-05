f07d37de3028a222fde5f454bec5e5486834add7
Ygor Pacheco Nogueira <ygor-nogueira@hotmail.com>
2021-12-05 19:59:37 -0300

    .....

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let e1 = document.querySelector('.menu-mobile-icon')
 
 e1.addEventListener('click', () => {
-  alert('clicado')
-})
+  
+  let menuItens = document.querySelector('.menu-itens');
+  menuItens.classList.add('show');
+});
